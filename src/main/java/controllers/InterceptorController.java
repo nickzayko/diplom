22e4dd804aa83ab5de822f2a69af1f6d24bcd5e2
nickzayko <karambannz@gmail.com>
2018-02-26 23:44:34 +0300

    commit

---
+++ src/main/java/controllers/InterceptorController.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import entity.UserEntity;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+public class InterceptorController implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        if (session.getAttribute("userId") == null && session.getAttribute("userName") == null) {
+            request.setAttribute("userEntity", new UserEntity());
+            request.setAttribute("informationAuthorization", "emptyString");
+            request.getRequestDispatcher("WEB-INF/pages/index.jsp").forward(request, response);
+        } else {
+            return true;
+        }
+        return true;
+    }
+
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}

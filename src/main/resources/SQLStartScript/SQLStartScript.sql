4ea651f7b6f6eee269738fc629cf036986b225e0
nickzayko <karambannz@gmail.com>
2018-03-17 14:23:45 +0300

    commit

--- src/main/resources/SQLStartScript/SQLStartScript.sql
+++ src/main/resources/SQLStartScript/SQLStartScript.sql
@@ -4,32 +4,32 @@ CREATE DATABASE IF NOT EXISTS diploma_project;
 
 USE diploma_project;
 
-CREATE TABLE diploma_project.users (
-  id_user INT NOT NULL AUTO_INCREMENT,
-  user_name VARCHAR(65) NOT NULL,
-  user_surname VARCHAR(65) NOT NULL,
-  user_email VARCHAR(65) NOT NULL,
-  user_login VARCHAR(65) NOT NULL,
-  user_password VARCHAR(65) NOT NULL,
-  PRIMARY KEY (id_users))
+CREATE TABLE 'diploma_project'.'users' (
+  'id_user' INT NOT NULL AUTO_INCREMENT,
+  'user_name' VARCHAR(65) NOT NULL,
+  'user_surname' VARCHAR(65) NOT NULL,
+  'user_email' VARCHAR(65) NOT NULL,
+  'user_login' VARCHAR(65) NOT NULL,
+  'user_password' VARCHAR(65) NOT NULL,
+  PRIMARY KEY ('id_users'))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE diploma_project.topics (
-  id_topics INT NOT NULL AUTO_INCREMENT,
-  topic_name VARCHAR(45) NOT NULL,
-  user_id INT NOT NULL,
-  PRIMARY KEY (id_topics))
+CREATE TABLE 'diploma_project'.'topics' (
+  'id_topics' INT NOT NULL AUTO_INCREMENT,
+  'topic_name' VARCHAR(45) NOT NULL,
+  'user_id' INT NOT NULL,
+  PRIMARY KEY ('id_topics'))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
-CREATE TABLE diploma_project.messages (
-  id_messages INT NOT NULL AUTO_INCREMENT,
-  text_of_message VARCHAR(250) NOT NULL,
-  user_id INT NOT NULL,
-  topic_id INT NOT NULL,
+CREATE TABLE 'diploma_project'.'messages' (
+  'id_messages' INT NOT NULL AUTO_INCREMENT,
+  'text_of_message' VARCHAR(250) NOT NULL,
+  'user_id' INT NOT NULL,
+  'topic_id' INT NOT NULL,
   localeDateTime DATETIME NOT NULL,
-  PRIMARY KEY (id_messages))
+  PRIMARY KEY ('id_messages'))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 

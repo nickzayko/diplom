059c2aaad252ff7fd9a4ca465ceb12a6428d154a
nickzayko <kolya-zayko@rambler.ru>
2020-04-07 21:55:22 +0300

    added google reCaptcha

--- src/main/java/controllers/AuthorizationController.java
+++ src/main/java/controllers/AuthorizationController.java
@@ -11,12 +11,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import service.UserServiceImpl;
+import utils.ReCaptchaValidator;
 import validators.AuthorizationValidator;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class AuthorizationController {
+
+    private String UUID = String.valueOf(java.util.UUID.randomUUID());
 
     @Autowired
     private UserServiceImpl userServiceImpl;
@@ -35,17 +39,27 @@ public class AuthorizationController {
 
     @RequestMapping(value = "/authorization", method = RequestMethod.POST)
     public String authorization(Model model, HttpSession session,
-                                @ModelAttribute("userEntity") UserEntity userEntity, BindingResult result) {
+                                @ModelAttribute("userEntity") UserEntity userEntity, BindingResult result,
+                                HttpServletRequest request) {
         authorizationValidator.validate(userEntity, result);
-        if (result.hasErrors()) {
+//        Из запроса получаем ReCaptcha
+        String gReCaptchaResponse = request.getParameter("g-recaptcha-response");
+        System.out.println("gReCaptchaResponse: " + gReCaptchaResponse);
+        boolean valid = ReCaptchaValidator.verify(gReCaptchaResponse);
+        System.out.println("valid: " + valid);
+
+        if (result.hasErrors() || !valid) {
             return "index";
+
         } else {
+
             userEntity = userServiceImpl.getUserByLoginAndPassword(userEntity.getUserLogin(), userEntity.getUserPassword());
             session.setAttribute("userId", userEntity.getIdUser());
             session.setAttribute("userName", userEntity.getUserName());
             model.addAttribute(userEntity);
             model.addAttribute("topicEntity", new TopicEntity());
             return "redirect:/menu/showMenu";
+
         }
     }
 
